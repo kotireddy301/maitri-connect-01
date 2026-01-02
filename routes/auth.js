f93365275722c22a689cfc2b8074959412f4029c
kotireddy301 <kotireddyambati301@gmail.com>
2026-01-02 15:19:13 +0530

    Fix: Switch to bcryptjs and add diagnostics

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const db = require('../db');
 const multer = require('multer');
